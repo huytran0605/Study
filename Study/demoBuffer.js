38f9962d642dcd524603041ea6cce6c6f61d0424
huy tran <huyaly0605@gmail.com>
2017-07-27 00:30:32 +0700

    Update new repo

---
+++ Study/demoBuffer.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var data = '';
+
+// Tao mot Readable Stream
+var readerStream = fs.createReadStream('config.json');
+
+// Thiet lap encoding la utf8. 
+readerStream.setEncoding('UTF8');
+
+// Xu ly cac su kien lien quan toi Stream --> data, end, va error
+readerStream.on('data', function(chunk) {
+   data += chunk;
+});
+
+readerStream.on('end',function(){
+    var buf = new Buffer(256);
+    var len = buf.write(data);
+    console.log( buf.toString('utf8',0,len));  
+});
+
+readerStream.on('error', function(err){
+   console.log(err.stack);
+});
